e0654f9033fe4d73821e3d7be0f67f1a86af7d2e
Hila Kershenovich <hilawol@gmail.com>
2021-03-10 20:00:03 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+class User {
+  constructor(id, firstName, lastName, capsule) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.capsule = capsule;
+    this.age = NAN;
+    this.city = undefined;
+    this.gender = undefined;
+    this.hobby = undefined;
+  }
+
+}
